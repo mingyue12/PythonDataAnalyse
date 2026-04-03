3034ca4fe1567ba0a2f07d74d424a17fff1eaa96
mingyue12 <108075525+mingyue12@users.noreply.github.com>
2026-04-03 15:34:44 +0800

    Update 06.窗口函数和自关联查询.sql
    yes

--- sql文件/06.窗口函数和自关联查询.sql
+++ sql文件/06.窗口函数和自关联查询.sql
@@ -18,12 +18,47 @@
         2.如果不写partition by，则默认对所有数据进行分组，如果写了，则对分组后的数据进行操作
         3.rows between 起始行 and 结束行：用于指定窗口的范围，即指定行的起始行和结束行
         4.如果不写order by，则默认对所有数据进行排序，如果写了，则对分组后的数据进行排序
+        5.over()还可以结合partition by和order by一起使用，还有count()、sum()、avg()、max()、min()、ntile()、lag()、lead(),first_value()、last_value()、nth_value()、cume_dist()、percent_rank()、last_value()等聚合函数
+    总结：关于窗口函数
+        1.分组排名
+        2.分组排名求TopN
  */
+# 准备数据
+create database day03;
+use day03;
+SHOW TABLES;
+
+# 建表，插入数据
+create table employee (empid int,ename varchar(20),dept_id int,salary decimal(10,2));
+
+insert into employee values(1,'刘备',10,5500.00);
+insert into employee values(2,'赵云',10,4500.00);
+insert into employee values(2,'张飞',10,3500.00);
+insert into employee values(2,'关羽',10,4500.00);
+
+insert into employee values(3,'曹操',20,19000.00);
+insert into employee values(4,'许褚',20,48000.00);
+insert into employee values(5,'张辽',20,45000.00);
+insert into employee values(6,'徐晃',20,14500.00);
+
+insert into employee values(7,'孙权',30,34500.00);
+insert into employee values(8,'周瑜',30,30650.00);
+insert into employee values(9,'陆逊',30,73000.00);
 
 
+# 查看数据
+select * from employee;
 
+# 案例一：分组排名
+# 需求：按照部门id分组，按照工资（salary）进行排名，降序。
+# 场景一：如何给表新增一列
+SELECT *, dept_id + 100 from employee;
 
-
+# 场景二：如何使用窗口函数
+SELECT
+    *,
+    RANK() OVER (PARTITION BY dept_id ORDER BY salary DESC) as rn
+FROM employee;
 
 
 
